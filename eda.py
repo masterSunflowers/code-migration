060620362a3b46f0ca025050c7f29e09239f8a8e
masterSunflowers <lvdthieu@gmail.com>
2024-12-09 21:12:01 +0700

    Merge code

--- eda.py
+++ eda.py
@@ -1,55 +1,466 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Init
+
+# In[1]:
+
+
 import pandas as pd
-import os
-import json
+import numpy as np
 
-processed2 = pd.read_csv("thieulvd/processed2.csv")
-
-
-for i in range(len(processed2)):
-    print(
-        eval(processed2.loc[i, "diff_files"])
-        if not pd.isna(processed2.loc[i, "diff_files"])
-        else None
-    )
-    print("=" * 100)
+seed = 18022004
+np.random.seed(seed)
 
 
-# # parsed_dir = "thieulvd/parsed"
-
-# # prev_method_count = 0
-# # cur_method_count = 0
-
-# # for _, row in processed2.iterrows():
-# #     if (
-# #         not pd.isna(row["java_only_diff_files"])
-# #         and row["java_only_diff_files"] != ""
-# #     ):
-# #         prev_identifier = row["repoName"] + "--" + row["prev_commit"]
-# #         cur_identifier = row["repoName"] + "--" + row["startCommit"]
-# #         methods_prev_commit = json.load(
-# #             open(os.path.join(parsed_dir, prev_identifier, "method_info.json"))
-# #         )
-# #         methods_cur_commit = json.load(
-# #             open(os.path.join(parsed_dir, cur_identifier, "method_info.json"))
-# #         )
-# #         prev_method_count += len(methods_prev_commit)
-# #         cur_method_count += len(methods_cur_commit)
-
-# # print(prev_method_count)
-# # print(cur_method_count)
-
-# df = pd.read_json("thieulvd/changed_methods.jsonl", lines=True)
-# print(df.info())
-# print(df.head())
-# print(df.describe())
-# print(df["repoName"].value_counts())
-# print(df["repoName"].value_counts().describe())
+# In[51]:
 
 
-# df_simplified = df.copy()
-# df_simplified["file_path"] = df["prev_method"].apply(lambda x: x["file_path"])
-# df_simplified["tree_path"] = df["prev_method"].apply(lambda x: x["tree_path"])
-# df_simplified["prev_method"] = df["prev_method"].apply(lambda x: x["definition"])
-# df_simplified["cur_method"] = df["cur_method"].apply(lambda x: x["definition"])
-# df_simplified.drop(columns=["diff_files", "java_only_diff_files"], inplace=True)
-# df_simplified.to_csv("thieulvd/changed_methods_simplified.csv", index=False)
+original_df = pd.read_parquet("official_original.parquet")
+original_df
+
+
+# In[3]:
+
+
+original_df["repoName"].nunique()
+
+
+# In[4]:
+
+
+tolib_test_df = original_df[original_df["toLib"].str.contains("unit|test")]
+
+tolib_test_df["repoName"].nunique()
+
+
+# In[5]:
+
+
+fromlib_test_df = original_df[original_df["fromLib"].str.contains("unit|test")]
+fromlib_test_df["repoName"].nunique()
+
+
+# In[6]:
+
+
+tolib_test_df
+
+
+# In[7]:
+
+
+fromlib_test_df
+
+
+# In[8]:
+
+
+print(tolib_test_df.index)
+
+
+# In[9]:
+
+
+print(fromlib_test_df.index)
+
+
+# In[10]:
+
+
+print(len(set(tolib_test_df.index).intersection(set(fromlib_test_df.index))))
+print(len(set(tolib_test_df.index).union(set(fromlib_test_df.index))))
+
+
+# In[11]:
+
+
+1297 / len(tolib_test_df)
+
+
+# In[12]:
+
+
+original_df[original_df["id"] == 3084].iloc[0]
+
+
+# In[13]:
+
+
+i = 3084
+
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# In[14]:
+
+
+i = 3085
+
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# In[15]:
+
+
+i = 3086
+
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# In[16]:
+
+
+i = 3087
+
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# In[17]:
+
+
+i = 3088
+
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# In[18]:
+
+
+set(tolib_test_df.index) - set(fromlib_test_df.index)
+
+
+# In[19]:
+
+
+set(fromlib_test_df.index) - set(tolib_test_df.index)
+
+
+# In[20]:
+
+
+i = 9158
+print(original_df.loc[i, "fromLib"])
+print(original_df.loc[i, "toLib"])
+print(original_df.loc[i, "repoName"])
+print(original_df.loc[i, "fileName"])
+
+
+# ***Cases that fromLib has different function than toLib***
+# 
+# - 1358
+# 
+# com.sun.jersey:jersey-bundle
+# 
+# org.glassfish.jersey.test-framework.providers:jersey-test-framework-provider-grizzly2
+# 
+# opensecuritycontroller_osc-core
+# 
+# osc-common/pom.xml
+# 
+# - 45
+# ant:ant
+# 
+# org.apache.ant:ant-junit
+# 
+# wocommunity_wolips
+# 
+# woproject-ant-tasks/pom.xml
+# 
+# - 3093
+# 
+# httpunit:httpunit
+# 
+# xerces:xercesImpl
+# 
+# apache_archiva
+# 
+# archiva-modules/archiva-web/archiva-webdav/pom.xml
+# 
+# - 5286
+# 
+# junit:junit
+# 
+# org.springframework.boot:spring-boot-starter-web
+# 
+# VIPJoey_doe
+# 
+# mmc-dubbo-provider/pom.xml
+# 
+# - 6432
+# 
+# net.htmlparser.jericho:jericho-html
+# 
+# net.sourceforge.htmlunit:htmlunit
+# 
+# apache_cxf-fediz
+# 
+# systests/tests/pom.xml
+# 
+# - 9158
+# 
+# org.dom4j:dom4j
+# 
+# org.xmlunit:xmlunit-matchers
+# 
+# apache_maven-archetype
+# 
+# archetype-common/pom.xml
+
+# ***Testing framework that not has test|unit***
+# com.jayway.restassured:rest-assured
+# 
+# org.robolectric:robolectric
+# 
+# io.rest-assured:rest-assured
+# 
+# jmock:jmock
+# 
+# org.mockito:mockito-core
+# 
+# org.assertj:assertj-core
+
+# In[21]:
+
+
+tolib_log_df = original_df[original_df["toLib"].str.contains("log|slf4j")]
+
+tolib_log_df["repoName"].nunique()
+
+
+# In[22]:
+
+
+fromlib_log_df = original_df[original_df["toLib"].str.contains("log|slf4j")]
+
+fromlib_log_df["repoName"].nunique()
+
+
+# In[23]:
+
+
+tolib_log_df
+
+
+# In[24]:
+
+
+fromlib_log_df
+
+
+# In[25]:
+
+
+print(tolib_log_df.index)
+print(fromlib_log_df.index)
+
+
+# In[26]:
+
+
+for lib in original_df["toLib"].sample(n=500):
+    print(lib)
+
+
+# In[27]:
+
+
+hard_remove_log_test_repos = (
+    set(original_df["repoName"].unique())
+    - set(fromlib_log_df["repoName"].unique())
+    - set(tolib_log_df["repoName"].unique())
+    - set(fromlib_test_df["repoName"].unique())
+    - set(tolib_test_df["repoName"].unique())
+)
+len(hard_remove_log_test_repos)
+
+
+# In[28]:
+
+
+# com.jayway.restassured:rest-assured
+# org.robolectric:robolectric
+# io.rest-assured:rest-assured
+# jmock:jmock
+# org.mockito:mockito-core
+# org.assertj:assertj-core
+
+
+# In[29]:
+
+
+fromlib_addition_test_df = original_df[
+    original_df["fromLib"].str.contains("rest-assured|robolectric|mock|assert")
+]
+fromlib_addition_test_df
+
+
+# In[30]:
+
+
+tolib_addition_test_df = original_df[
+    original_df["toLib"].str.contains("rest-assured|robolectric|mock|assert")
+]
+tolib_addition_test_df
+
+
+# In[31]:
+
+
+hard_remove_log_test_repos = (
+    hard_remove_log_test_repos
+    - set(fromlib_addition_test_df["repoName"].unique())
+    - set(tolib_addition_test_df["repoName"].unique())
+)
+print(len(hard_remove_log_test_repos))
+
+
+# In[32]:
+
+
+hard_remove_log_test_repos
+
+
+# In[33]:
+
+
+hard_remove_log_test_df = original_df[
+    original_df["repoName"].isin(hard_remove_log_test_repos)
+]
+hard_remove_log_test_df
+
+
+# In[34]:
+
+
+migration_in_repo = dict(hard_remove_log_test_df["repoName"].value_counts())
+
+
+# In[35]:
+
+
+len(list(filter(lambda x: migration_in_repo[x] > 1, migration_in_repo)))
+
+
+# In[36]:
+
+
+repo_with_only_one_migration = list(
+    filter(lambda x: migration_in_repo[x] == 1, migration_in_repo)
+)
+repo_with_only_one_migration
+
+
+# In[37]:
+
+
+len(repo_with_only_one_migration)
+
+
+# In[38]:
+
+
+check = dict(original_df["repoName"].value_counts())
+check
+
+
+# In[39]:
+
+
+len(list(filter(lambda x: check[x] > 1, check)))
+
+
+# In[40]:
+
+
+982 / 1480
+
+
+# In[41]:
+
+
+one_df = original_df[original_df["repoName"].isin(repo_with_only_one_migration)]
+one_df
+
+
+# In[42]:
+
+
+one_df.to_csv("demo.csv", index=False)
+
+
+# In[43]:
+
+
+sampled_df = one_df.sample(n=20, random_state=42)
+sampled_df
+
+
+# In[44]:
+
+
+sampled_df.to_csv("sampled.csv", index=False)
+
+
+# In[45]:
+
+
+(one_df["startCommit"] == one_df["endCommit"]).value_counts()
+
+
+# In[46]:
+
+
+(sampled_df["startCommit"] == sampled_df["endCommit"]).value_counts()
+
+
+# In[56]:
+
+
+original_df["num_migration"] = original_df.groupby("repoName")["repoName"].transform(
+    "count"
+)
+original_df
+
+
+# In[57]:
+
+
+original_df["num_migration"]
+
+
+# In[58]:
+
+
+13382 / 1480
+
+
+# In[59]:
+
+
+check = original_df.drop_duplicates(subset=["repoName"])
+check.describe()
+
+
+# # Check file change
+
+# In[ ]:
+
+
+
+
---
+++ note.md
@@ -0,0 +1,58 @@
+# Insight
+
+1. Differences in libraries function
+
+    1358
+
+    com.sun.jersey:jersey-bundle
+    org.glassfish.jersey.test-framework.providers:jersey-test-framework-provider-grizzly2
+    opensecuritycontroller_osc-core
+    osc-common/pom.xml
+
+    45
+
+    ant:ant
+    org.apache.ant:ant-junit
+    wocommunity_wolips
+    woproject-ant-tasks/pom.xml
+
+    3093
+
+    httpunit:httpunit
+    xerces:xercesImpl
+    apache_archiva
+    archiva-modules/archiva-web/archiva-webdav/pom.xml
+
+    5286
+
+    junit:junit
+    org.springframework.boot:spring-boot-starter-web
+    VIPJoey_doe
+    mmc-dubbo-provider/pom.xml
+
+    6432
+
+    net.htmlparser.jericho:jericho-html
+    net.sourceforge.htmlunit:htmlunit
+    apache_cxf-fediz
+    systests/tests/pom.xml
+
+    9158
+
+    org.dom4j:dom4j
+    org.xmlunit:xmlunit-matchers
+    apache_maven-archetype
+    archetype-common/pom.xml
+
+2. Some test lib without test, unit in name
+
+    com.jayway.restassured:rest-assured
+    org.robolectric:robolectric
+    io.rest-assured:rest-assured
+    jmock:jmock
+    org.mockito:mockito-core
+    org.assertj:assertj-core
+
+3. Much repo has multiple migrations
+
+4. Repo that start/end commit does not exist
